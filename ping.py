0856f2118bf6b332b6bbffc70b964fc899b0b5b6
Graham <graham@rvvup.com>
2024-06-01 08:07:30 +0100

    using dotenv file for the token

--- ping.py
+++ ping.py
@@ -1,13 +1,10 @@
-import os
+from dotenv import load_dotenv
 
 from rvvup import RvvupClient
 
-token = os.environ["RVVUP_API_KEY"]
+load_dotenv()
 
-if token is None or len(token) == 0:
-    raise ValueError("RVVUP_API_KEY environment variable must be set")
-
-client = RvvupClient(auth_token=token, user_agent="ping/1.0")
+client = RvvupClient()
 
 response = client.ping()
 print(f"Pong received at: {response}")
